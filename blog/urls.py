c0430e0d148ec4a3e6dd257a33b5a4f94d73bcfe
cosmosmagic <51026483+cosmosmagic@users.noreply.github.com>
2019-10-15 05:55:39 +0800

    add user avatar

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from myblog.feeds import AllBlogsRssFeed
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('search/', include('haystack.urls')),
     path('accounts/',include('users.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
